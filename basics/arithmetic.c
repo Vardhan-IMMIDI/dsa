967b58793e12470850f0c5a1174bcbd36d9c7084
IMMIDI NAGA VIJAY VARDHAN GUPTA <90963630+Vardhan-IMMIDI@users.noreply.github.com>
2025-11-26 15:27:35 +0530

    arithmetic

---
+++ basics/arithmetic.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+int main (void)
+{
+    int a = 10, b = 20;
+    printf("Arithmetic Operators Demo: \n");
+    printf("a + b = %i\na - b = %i\na / b = %i\na * b = %d\na %% b = %d\n", a + b, a - b, a / b, a * b, a % b);
+}
