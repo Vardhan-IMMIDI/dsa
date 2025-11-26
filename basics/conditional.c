8f61b867bef3a017f4198db5d58bb16562c3558d
IMMIDI NAGA VIJAY VARDHAN GUPTA <90963630+Vardhan-IMMIDI@users.noreply.github.com>
2025-11-26 15:31:40 +0530

    conditional

---
+++ basics/conditional.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main (void)
+{
+    int a = 10;
+
+    if (a > 10)
+    {
+        printf("A is greater than Ten\n");
+    }
+    else if (a < 10)
+    {
+        printf("A i sless than Ten\n");
+    }
+    else
+    {
+        printf("A is equal to Ten\n");
+    }
+}
