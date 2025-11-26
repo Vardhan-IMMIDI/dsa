12744d8fa6c65995361795b868b9db183405934a
IMMIDI NAGA VIJAY VARDHAN GUPTA <90963630+Vardhan-IMMIDI@users.noreply.github.com>
2025-11-26 15:21:11 +0530

    Data Types

---
+++ basics/datatypes.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main (void)
+{
+    char c = 'C';
+    short s = 20;
+    int i = 30;
+    long l = 40;
+    float f = 10.05;
+    double d = 20.50;
+    long double ld = 10.3054;
+    printf("Data Types: \n");
+    printf("Char: %c\nShort: %i\nInt: %i\nLong: %li\nFloat: %f\nDouble: %lf\nLong Double: %Lf\n", c, s, i, l, f, d, ld);
+
+}
