f15ffb793a34682749b51755d85db4c3f5f9edc7
IMMIDI NAGA VIJAY VARDHAN GUPTA <90963630+Vardhan-IMMIDI@users.noreply.github.com>
2025-11-28 00:22:22 +0530

    arrays rotate

--- arrays/arrays.c
+++ arrays/arrays.c
@@ -1,14 +1,33 @@
 #include <stdio.h>
 #include <stdbool.h>
 
+void display (int arr[], int n);
+void rotate (int arr[], int n, int by);
+
+
 int main (void)
 {
-    printf("");
+    int arr[] = {1, 2, 3, 4, 5};
+    display(arr, 5);
+    rotate(arr, 5, -3);
+    display(arr, 5);
+    rotate(arr, 5, 3);
+    display(arr, 5);
+    
 }
 
-int min (int arr[])
+void display (int arr[], int n)
 {
-    int n = sizeof(arr) / sizeof(arr[0]);
+    printf("Array: ");
+    for (int i = 0; i < n; i++)
+    {
+        printf("%d ", arr[i]);
+    }
+    printf("\n");
+}
+
+int max (int arr[], int n)
+{
     int max = arr[0];
 
     for (int i = 0; i < n; i++)
@@ -22,9 +41,8 @@ int min (int arr[])
     return max;
 }
 
-int min (int arr[])
+int min (int arr[], int n)
 {
-    int n = sizeof(arr) / sizeof(arr[0]);
     int min = arr[0];
 
     for (int i = 0; i < n; i++)
@@ -38,9 +56,8 @@ int min (int arr[])
     return min;
 }
 
-int sum (int arr[])
+int sum (int arr[], int n)
 {
-    int n = sizeof(arr) / sizeof(arr[0]);
     int sum = 0;
 
     for (int i = 0; i < n; i++)
@@ -51,9 +68,8 @@ int sum (int arr[])
     return sum;
 }
 
-int avg (int arr[])
+int avg (int arr[], int n)
 {
-    int n = sizeof(arr) / sizeof(arr[0]);
     int sum = 0;
 
     for (int i = 0; i < n; i++)
@@ -64,10 +80,8 @@ int avg (int arr[])
     return sum / n;
 }
 
-bool linear_search (int arr[], int key)
+bool linear_search (int arr[], int n, int key)
 {
-    int n = sizeof(arr) / size(arr[0]);
-
     for (int i = 0; i < n; i++)
     {
         if (arr[i] == key)
@@ -77,4 +91,55 @@ bool linear_search (int arr[], int key)
     }
 
     return false;
+}
+
+bool binary_search (int arr[], int n, int key)
+{
+    int low = 0, high = n;
+
+    while (low <= high)
+    {
+        int mid = (low + high) / 2;
+        if (arr[mid] == key)
+        {
+            return true;
+        }
+        else if (arr[mid] > key)
+        {
+            high = mid - 1;
+        }
+        else if (arr[mid] < key)
+        {
+            low = mid + 1;
+        }
+    }
+    return false;
+}
+
+void rev (int arr[], int n)
+{
+    for (int i = 0; i < n / 2; i++)
+    {
+        arr[i] = arr[n - i - 1];
+    }
+}
+
+void rotate (int arr[], int n, int by)
+{
+    int temp[n];
+
+    for (int i = 0; i < n; i++)
+    {
+        temp[i] = arr[i];
+    }
+
+    for (int i = 0; i < n; i++)
+    {
+        int index = (i + by) % n;
+        if (index < 0)
+        {
+            index = n + index;
+        }
+        arr[i] = temp[index];
+    }
 }
